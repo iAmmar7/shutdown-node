52101a15a80816ebf774f55f4d68247e796294b0
iammar7 <maansaridot@gmail.com>
2020-06-17 12:18:37 +0500

    image open feature added

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+// Require child_process
+var exec = require('child_process').exec;
+
+// Create shutdown function
+function shutdown(callback) {
+  console.log('Am i invoking?');
+  exec(`ReactComponents.png`, function (error, stdout, stderr) {
+    callback(stdout);
+  });
+}
+
+app.get('/', (req, res) => {
+  res.send('Buckle up! You are about to fly');
+  // Reboot computer
+  shutdown(function (output) {
+    console.log(output);
+  });
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
